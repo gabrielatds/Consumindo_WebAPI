65afa0a0f255025e1f7bc5dec6671649b944084a
Gabriel <gabriel_3404@hotmail.com>
2020-08-25 21:26:33 -0300

    criando projeto, model, controller, view

---
+++ Controllers/EstadoFazendaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Consumindo_WebAPI.Models;
+using System.Net.Http;
+
+namespace Consumindo_WebAPI.Controllers
+{
+    public class EstadoFazendaController : Controller
+    {
+        //GET: EstadoFazenda
+        public IActionResult Index()
+        {
+            IEnumerable<EstadoFazenda> estadoFazenda = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://tst.sportibrasil.com.br/Services/CategoriaOficialService.svc/ObterPorEstadoFazenda/2 ");
+                var responseTask = client.GetAsync("estadoFazenda");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var read = result.Content.ReadAsAsync< IList < EstadoFazenda >>();
+                    read.Wait();
+                    estadoFazenda = read.Result;
+                }
+                else
+                {
+                    estadoFazenda = Enumerable.Empty<EstadoFazenda>();
+                    ModelState.AddModelError(string.Empty, "Server error ocurred");
+                }
+            }
+
+            return View(estadoFazenda);
+        }
+    }
+}
---
+++ Models/EstadoFazenda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consumindo_WebAPI.Models
+{
+    public class EstadoFazenda
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string UF { get; set; }
+        public int ValorMax { get; set; }
+        public int ValorMin { get; set; }
+    }
+}
